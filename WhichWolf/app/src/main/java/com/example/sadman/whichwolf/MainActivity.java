0003d666c6fbeba1d36ba922bbb0f151ab7e8ce7
shabab ayub <shabab ayub>
2016-04-02 19:16:32 -0400

    Camera works, gotta get to next activity

--- WhichWolf/app/src/main/java/com/example/sadman/whichwolf/MainActivity.java
+++ WhichWolf/app/src/main/java/com/example/sadman/whichwolf/MainActivity.java
@@ -1,6 +1,7 @@
-package com.example.sadman.whichwolf;;
+package com.example.sadman.whichwolf;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -11,22 +12,38 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.Toast;
+import android.widget.TextView;
+import java.lang.Object;
+
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv1;
     private static final int startCameraCode = 0; //this is an identifier when you request the camera app
     private ImageView mPhotoCapturedImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // for hidings
+
+
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         mPhotoCapturedImageView = (ImageView) findViewById(R.id.capturePhotoImageView);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        tv1=(TextView)findViewById(R.id.text1);
+
+        Typeface face= Typeface.createFromAsset(getAssets(), "lobster.ttf");
+        tv1.setTypeface(face);
+
+
+
+       /* Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -36,30 +53,11 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+
+        });*/
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     public void takePhoto(View view){
         Intent callCameraApp = new Intent();
@@ -67,36 +65,13 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(callCameraApp, startCameraCode);//first param is the intent, second is the identifier
     }
     //the following function intercepts the photo captured by the camera
-    public void onActivityResult(int requestCode, int resultCode, Intent data){
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
         if(requestCode == startCameraCode && resultCode == RESULT_OK){
             Bundle extras = data.getExtras(); //formatting the DATA
             Bitmap photoBitmap = (Bitmap) extras.get("data");  //THIS GIVES US THE THUMBNAIL RETURNED BY THE CAMERA ACTIVITY!
             mPhotoCapturedImageView.setImageBitmap(photoBitmap);
             Toast.makeText(MainActivity.this, "Photo taken!", Toast.LENGTH_SHORT).show();
         }
-        //else if (requestCode == )
     }
-    //the following function was made to store the actual full size image (maybe the thumbnail is sufficient) HOW TO IMPORT FILE CLASS
-    /*void createImageFile (){
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()); //allows files to have unique names
-        String fileName = "IMAGE_" + timeStamp + "_";
-        File storageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
 
-       // .... Do we even need to do this!??!?!?
-
-    }*/
-/*
-    //ACCESSING THE GALLERY NOT SURE IF THIS REALLY WORKS OR NAH
-
-    public void useGallery(View view){
-        Intent callGallery = new Intent(Intent.ACTION_PICK,
-                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(callGallery, startCameraCode);//first param is the intent, second is the identifier
-    }
-*/
-    public void useGallery(View view) {
-        Intent callGallery = new Intent(Intent.ACTION_PICK,
-                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        //startActivityForResult(callGallery, );
-    }
 }
